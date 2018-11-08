f836cb528fe5cb617d7e53e13d6d5f26b651e469
Luis Soria <luismiguel.ca@gmail.com>
2018-11-08 16:01:48 +0100

    inicial

---
+++ scrolling_matrix_fix.js
@@ -0,0 +1,15 @@
+$(document).ready( function() {
+
+console.log("hola mundo");
+$("#div_213150, div.carroussel, div.topContent, ul,ulli, p.bottom").css("background-color","#ffffff");
+    
+$("#div_213150, div.carroussel, div.topContent, ul,ulli, p.bottom").attr("style","background-color: #ffffff !important;");
+
+$(".ui-state-default, .ui-widget-content, .ui-state-default, .ui-widget-header, .ui-state-default").find("a").attr("style","background:#000000 !important;");
+
+$("div.carroussel, div.topContent, ul, li").find("div.container").attr("style","border:0px;background-color:transparent !important;"); 
+
+$("#div_213150, div.carroussel").find(".list").attr("style","border:0px;background-color:transparent !important;"); 
+$(".list").find("li").attr("style","border:0px;background-color:transparent !important;");
+$(".list").find("li").attr("style","border:0px;background:transparent !important;"); 
+});
