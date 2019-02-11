3c00b0c3894adbc49a4af3d9d1bddb02be63497e
Luis Soria <luismiguel.ca@gmail.com>
2019-02-11 17:39:59 +0100

    Create cambio_de _color.js

---
+++ cambio_de _color.js
@@ -0,0 +1,4 @@
+$(".step2").find("p:first-child").css({"background-color":"white","color":"#3399ff","font-style":"italic"});
+$(".step3").find("p:first-child").css({"background-color":"white","color":"#3399ff","font-style":"italic"});
+$(".step4").find("p:first-child").css({"background-color":"white","color":"#3399ff","font-style":"italic"});
+$(".step5").find("p:first-child").css({"background-color":"white","color":"#3399ff","font-style":"italic"});
